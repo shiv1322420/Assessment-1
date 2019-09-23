d780eba1cc4bd86f702ce94424f484c734d3834c
shiv1322420 <55525140+shiv1322420@users.noreply.github.com>
2019-09-23 10:53:34 +0530

    Update email_val.js

--- email_val.js
+++ email_val.js
@@ -1,6 +1,6 @@
 //program to validate the email address
 
-var email="sk623520gmail.co.in";                                             //intialization of string email
+var email="sk623520@gmail.co.in";                                             //intialization of string email
 var reg=/^([a-z\d\.-]+)@([a-z]{2,10}[.])([a-z]{2,10})(.[a-z]{2,10})?$/g     //intialization of regular expresstion
 if(reg.test(email)==true)                                                   //test() validate the string that is paased as argument with regex: and it resturns true or false
 {
